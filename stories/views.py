a59c8e5212ed4238fe9b72c284c78a53ca5d5390
fouziaa62 <fouziaahmad62@gmail.com>
2025-03-05 12:33:49 +0000

    added story template

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Story
 
 # Create your views here.
+# This is a view function that returns a list of stories.
+def story_list(request):
+    stories = Story.objects.all().order_by('-created_at')
+    return render(request, 'stories/story_list.html', {'stories': stories})
+# this is a view function that returns a single story.  
+def story_detail(request, story_id):
+    story = get_object_or_404(Story, id=story_id)
+    return render(request, 'stories/story_detail.html', {'story': story})
