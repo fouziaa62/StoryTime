9ce2b91fe2fe73cdec805534ad7f01a6bf4af9d7
fouziaa62 <fouziaahmad62@gmail.com>
2025-03-08 12:14:36 +0000

    added css and bootstrap

--- stories/urls.py
+++ stories/urls.py
@@ -3,10 +3,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.story_list, name='story_list'),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('story_list/', views.story_list, name='story_list'),
     path('story/<int:story_id>/', views.story_detail, name='story_detail'),
     path('signup/', views.signup, name='signup'),
-    path('home/', views.home, name='home'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
