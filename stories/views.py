ffad8e90bb4acadefdb25474b58cf28c6d4a3944
fouziaa62 <fouziaahmad62@gmail.com>
2025-03-08 11:49:11 +0000

    story editing and deleting fixed

--- stories/views.py
+++ stories/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Story
 from .forms import UserSignupForm, StoryForm
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 # Create your views here.
 @login_required
