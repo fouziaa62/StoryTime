70e7e03b0d3f24823754d8029d6e8e99dd29cd64
fouziaa62 <fouziaahmad62@gmail.com>
2025-03-23 13:55:05 +0000

    debugged with copilot

--- storyshare/settings.py
+++ storyshare/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'https://stories-724c424e708b.herokuapp.com/',
